54d1c97cf6b1bd26a2df26aa233dd890f7646108
MaxNumerique <maximilienlandois@gmail.com>
2025-09-15 16:11:17 +0200

    renamed var database & cleaned imports and README

--- src/opengeodeweb_microservice/database/connection.py
+++ src/opengeodeweb_microservice/database/connection.py
@@ -8,22 +8,22 @@ from flask_sqlalchemy.session import Session
 from .base import Base
 
 DATABASE_FILENAME = "project.db"
-db: Optional[SQLAlchemy] = None
+database: Optional[SQLAlchemy] = None
 
 
 def init_database(app: Flask, db_filename: str = DATABASE_FILENAME) -> SQLAlchemy:
-    global db
-    if db is None:
-        db = SQLAlchemy(model_class=Base)
-    db.init_app(app)
+    global database
+    if database is None:
+        database = SQLAlchemy(model_class=Base)
+    database.init_app(app)
     with app.app_context():
-        db.create_all()
-    return db
+        database.create_all()
+    return database
 
 
 def get_database() -> Optional[SQLAlchemy]:
-    return db
+    return database
 
 
 def get_session() -> Optional[scoped_session[Session]]:
-    return db.session if db else None
+    return database.session if database else None
